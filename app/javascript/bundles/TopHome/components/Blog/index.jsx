244c7a951df21b89426620a981cda1ba3ac046dd
topcbl <topcbl@gmail.com>
2018-07-29 23:59:46 +0700

    Added markdown editor.

--- app/javascript/bundles/TopHome/components/Blog/index.jsx
+++ app/javascript/bundles/TopHome/components/Blog/index.jsx
@@ -1,11 +1,20 @@
 // @flow
 import React from "react"
+import SimpleMDE from "simplemde"
 
 type Props = {}
 type State = {}
 
 export default class Blogs extends React.Component<Props, State> {
   render () {
-    return <div>dfdf</div>
+    return (
+      <div>
+        <textarea id="MyID"/>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
   }
 }
