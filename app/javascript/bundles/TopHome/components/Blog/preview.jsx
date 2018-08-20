9ac5b7854efcd7b1b08914c8e6bf532642b2e983
LongCB <chau.bao.long@framgia.com>
2018-08-20 11:23:09 +0700

    Responsive blogs component.

--- app/javascript/bundles/TopHome/components/Blog/preview.jsx
+++ app/javascript/bundles/TopHome/components/Blog/preview.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React from "react"
 import type { Blog } from "../../services/restClient/models/blog"
+import ReactMarkdown from "react-markdown"
 
 type Props = {
   blog: Blog,
@@ -9,21 +10,19 @@ type Props = {
 export default function Preview({blog}: Props) {
   return (
     <div className="col-md-6">
-      <div className="preview">
-        <span>
+      <div className="row preview">
+        <span className="col-md-4 col-xs-12 preview__img-container">
           <img 
             className="preview__img"
             src="https://cdn-images-1.medium.com/max/800/1*ypedtlNYHDd94HwB1f-AnQ.jpeg"
           />
         </span>
-        <div className="preview__title">
-          This is ttest title
-        </div>
-        <div className="preview__content">
-          Contentk;sdfjldksjfs;dlfjsd;<br/>
-          lkfjsd;lkfjsd;lfkjsdf;lksdjf;lsdkjfsdl;kfjslksfj<br/>
-          dfdfdf
-        </div>
+        <span className="col-md-8 preview__blog">
+          <div className="preview__title">
+            {blog.title}
+          </div>
+          <ReactMarkdown className="preview__content" source={blog.body}/>
+        </span>
       </div>
     </div>
   )
